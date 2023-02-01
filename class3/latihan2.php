7740ea17150260049a7c3b44a47072d2e23c775b
Glenn Mandagi <s21710491@student.unklab.ac.id>
2023-02-01 17:45:43 +0800

    Add files via upload

---
+++ class3/latihan2.php
@@ -0,0 +1,11 @@
+<?php
+
+$nama_barang = "Minyak Goreng";
+$harga = 15000;
+
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+
+$judul1 = "Belajar PHP dari nol sampai expert";
+echo "Judul artikel: $judul1;"
+
+?>
